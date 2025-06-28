c6e7d3ba8f80b1a4e214eb9fec7fb555ea5cdad1
AllMeatball <181806857+AllMeatball@users.noreply.github.com>
2025-06-28 02:51:39 -0500

    Expose more functions in LegoVideoManager

--- LEGO1/lego/legoomni/include/legovideomanager.h
+++ LEGO1/lego/legoomni/include/legovideomanager.h
@@ -139,7 +139,26 @@ inline void VideoManager_SolWrap(sol::state &p_lua)
 {
 	sol::usertype<LegoVideoManager> videomanager_type = p_lua.new_usertype<LegoVideoManager>(
 		"LegoVideoManager",
-		"EnableFullScreenMovieWithScale", &LegoVideoManager::EnableFullScreenMovieWithScale
+		"EnableFullScreenMovieWithScale",
+		&LegoVideoManager::EnableFullScreenMovieWithScale,
+
+		"SetRender3D",
+		&LegoVideoManager::SetRender3D,
+
+		"GetRender3D",
+		&LegoVideoManager::GetRender3D,
+
+		"SetSkyColor",
+		&LegoVideoManager::SetSkyColor,
+
+		"ToggleFPS",
+		&LegoVideoManager::ToggleFPS,
+
+		"Get3DManager",
+		&LegoVideoManager::Get3DManager,
+
+		"GetRenderer",
+		&LegoVideoManager::GetRenderer
 	);
 
 	p_lua["VideoManager"] = &VideoManager;
